66a3b1aa78cabe9211622cecaa6cd6acd2a34b2c
Ben <benmitchell@Bens-MacBook-Air.local>
2020-04-24 18:01:01 -0500

    adding buying/selling features

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db,login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+import xlrd
 
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,11 +25,77 @@ class Portfolio(db.Model):
     teams = db.Column(db.String(140)) # possibly a foreign key to another Teams class
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    cash = db.Column(db.Integer)
+    cash = db.Column(db.Integer, default = 10000)
 
     def __repr__(self):
         return '<Portfolio {}>'.format(self.teams)
 
+class Teams(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    all_teams = db.Column(db.String(140))
+    price = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<Teams {}>'.format(self.all_teams)
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
+def add_teamprice():
+    excelfile = 'prices.xlsx'
+    wb = xlrd.open_workbook(excelfile)
+    sheet = wb.sheet_by_index(0)
+    sheet.cell_value(0, 0)
+    teampricelist = []
+    teamslist = []
+    priceslist = []
+    for i in range(1, 31):
+        teams = sheet.cell_value(i, 0)
+        prices = (sheet.cell_value(i, 17))
+        teamslist.append(teams)
+        priceslist.append(prices)
+        all_teamsandprices = Teams(all_teams = teams, price = prices)
+        db.session.add(all_teamsandprices)
+
+    db.session.commit()
+
+def delete_teamprice():
+    deleting = Teams.query.first()
+    #all_teams = db.session.query(Teams.all_teams)
+    #price = db.session.query(Teams.price)
+    db.session.delete(deleting)
+    db.session.commit()
+
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+delete_teamprice()
+
+add_teamprice()
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,13 @@
 from flask import render_template, flash, redirect, request, url_for
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, BuyForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User, Portfolio
+from app.models import User, Portfolio, Teams
 from flask_login import login_required
 from flask import request, g
 from werkzeug.urls import url_parse
 import xlrd
+import sqlalchemy
 
 @app.before_request
 def global_user():
@@ -19,23 +20,53 @@ def template_user():
         return {'user': g.user}
     except AttributeError:
         return {'user': None}
+        
 
-
-@app.route('/')
-@app.route('/index')
-@login_required
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+@login_required 
 def index():
-    excelfile = 'teamprices.xlsx'
+    cash = db.session.query(Portfolio.cash).filter_by(user_id=current_user.id).first()
+    excelfile = 'prices.xlsx'
     wb = xlrd.open_workbook(excelfile)
     sheet = wb.sheet_by_index(0)
     sheet.cell_value(0, 0)
+    teampricelist = []
     teamslist = []
-    for i in range(31): 
+    priceslist = []
+    for i in range(1, 31):
         teams = sheet.cell_value(i, 0)
         prices = str(sheet.cell_value(i, 17))
-        teamslist.append(teams + " : " + "$" + prices)
+        teamslist.append(teams)
+        priceslist.append(prices)
+        teampricelist.append(teams + " : " + "$" + prices)
 
-    return render_template('index.html', title='Home', teamslist = teamslist)
+
+    all_teamprice = db.session.query(Teams).all()
+    all_teams = db.session.query(Teams.all_teams)
+    price = db.session.query(Teams.price)
+
+
+
+
+
+    teamssssss = [(i.price, i.all_teams) for i in all_teamprice]
+    
+    form = BuyForm()
+    form.buy.choices = teamssssss
+
+
+    if form.validate_on_submit():
+        flash('buyers beware')
+        price = form.quantity.data * form.buy.data
+        print(form.quantity.data)
+        print(form.buy.data)
+        print(price)
+        new_cash = Portfolio(cash = (cash - price))
+        print(new_cash)
+        db.session.append(new_cash)
+
+    return render_template('index.html', title='Home',teampricelist = teampricelist, form = form)
 
 # logs users in
 @app.route('/login', methods=['GET', 'POST'])
